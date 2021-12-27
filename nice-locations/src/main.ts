e247dcde989860f676fb809a191153b93fb3c00b
DangLyPTN <dang.ly@ptnglobalcorp.com>
2021-12-27 14:00:37 +0700

    project nice-locations

---
+++ nice-locations/src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { resolve } from 'path';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const APP_DIRECTORY = resolve(__dirname, '..');
+
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setViewEngine('hbs');
+  app.setBaseViewsDir(resolve(APP_DIRECTORY, 'views'));
+  app.useStaticAssets(resolve(APP_DIRECTORY, 'public'));
+
+  await app.listen(3000);
+}
+bootstrap();
