9a6b11e197d8b381c3dd435f08d8b4fa3797223d
DangLyPTN <dang.ly@ptnglobalcorp.com>
2021-12-28 11:00:59 +0700

    Finish lesson 19

--- nice-locations/config/default.ts
+++ nice-locations/config/default.ts
@@ -1,6 +1,15 @@
 import { resolve } from 'path';
 
+const SOURCE_PATH = resolve(__dirname, '..', 'src');
+
 export default {
+  database: {
+    url: 'postgres://app:app@localhost/locations',
+  },
+  orm: {
+    entities: [`${SOURCE_PATH}/**/*.entity.ts`],
+    synchronize: false,
+  },
   public: {
     path: resolve(__dirname, '..', 'public'),
   },
